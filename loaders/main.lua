bb89f58aa76f61fa014a5ba394381e4ccc3d5980
legit <danielabrantesrzz@gmail.com>
2026-01-12 10:51:58 -0300

    ...

--- loaders/main.lua
+++ loaders/main.lua
@@ -1,1 +1,12 @@
+local placeID = game.PlaceId
+local gameID = game.GameId
 
+if placeID == 893973440 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/nextydev/Snw-Hub/refs/heads/main/ftf0.0.1.lua"))()
+elseif gameID == 6931042565 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/nextydev/Snw-Hub/refs/heads/main/vbl0.0.1.lua"))()
+else
+    print("[SnwHub] Game not supported!")
+    print("[SnwHub] Join Discord for see the games supported!")
+    setclipboard("discord.gg/J7X36yv9hx")
+end
